277ef269ec37729c7df4704fbb6d7bb2ebad05eb
fasikaWalle <fasikabini12@gmail.com>
2021-04-27 23:25:27 -0500

    create indexDB connection

---
+++ public/js/idb.js
@@ -0,0 +1,21 @@
+
+let db;
+const request=indexedDB.open('budgt_tracker',1)
+
+request.onupgradeneeded=function(event){
+    const db=event.target.result;
+    db.createObjectStore('new_transaction',{autoIncrement:true})
+}
+
+request.onsuccess=function(event){
+    db=event.target.result
+    if(navigator.onLine){
+        uploadTransaction()
+    }
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
+  
+  
