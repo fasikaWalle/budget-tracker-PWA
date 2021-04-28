a8cfaf887e0b332bec62739aa964b110c1b5939c
fasikaWalle <fasikabini12@gmail.com>
2021-04-27 23:26:33 -0500

    save transaction to indexDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,4 +18,44 @@ request.onerror = function(event) {
     console.log(event.target.errorCode);
   };
   
+  function uploadTransaction(){
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+   
+ getAll.onsuccess = function() {
+     if (getAll.result.length > 0) {
+       fetch('/api/transaction', {
+         method: 'POST',
+         body: JSON.stringify(getAll.result),
+         headers: {
+           Accept: 'application/json, text/plain, */*',
+           'Content-Type': 'application/json'
+         }
+       })
+         .then(response => response.json())
+         .then(serverResponse => {
+           if (serverResponse.message) {
+             throw new Error(serverResponse);
+           }
+        
+           const transaction = db.transaction(['new_transaction'], 'readwrite');
+           
+           const transactionObjectStore = transaction.objectStore('new_transaction');
+          
+           transactionObjectStore.clear();
+ 
+           alert('All saved transaction has been saved!!');
+         })
+         .catch(err => {
+           console.log(err);
+         });
+     }
+   };
+ }
+ 
+ window.addEventListener('online',uploadTransaction)
+  
   
